67730b67145c187afeea735584552542989934d0
SubhamPramanik <subhampramanik@gmail.com>
2020-06-16 21:35:12 +0530

    probaly zeebe config

--- src/main/java/org/mifos/connector/ams/zeebe/ZeebeClientConfiguration.java
+++ src/main/java/org/mifos/connector/ams/zeebe/ZeebeClientConfiguration.java
@@ -1,6 +1,8 @@
 package org.mifos.connector.ams.zeebe;
 
 import io.zeebe.client.ZeebeClient;
+import io.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
+import io.zeebe.client.impl.oauth.OAuthCredentialsProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,9 +18,17 @@ public class ZeebeClientConfiguration {
 
     @Bean
     public ZeebeClient setup() {
+        OAuthCredentialsProvider cred = new OAuthCredentialsProviderBuilder()
+                .audience("4ae8d42c-5f02-4e18-b4d4-f4d3d92fc824.zeebe.camunda.io")
+                .clientId("ruU.ptno_wct_PXW~ngW4Pp0A0QWw.E1")
+                .clientSecret("is20pecUw4p_9s3DakWcayYPdDn.GhWfmkm7jtznSErP.ed1iR.z.Qg97EBVz~Pc")
+                .authorizationServerUrl("https://login.cloud.camunda.io/oauth/token")
+                .build();
+
         return ZeebeClient.newClientBuilder()
-                .brokerContactPoint(zeebeBrokerContactpoint)
-                .usePlaintext()
+                .brokerContactPoint("4ae8d42c-5f02-4e18-b4d4-f4d3d92fc824.zeebe.camunda.io:443")
+                .credentialsProvider(cred)
+                // .usePlaintext()
                 .numJobWorkerExecutionThreads(zeebeClientMaxThreads)
                 .build();
     }
